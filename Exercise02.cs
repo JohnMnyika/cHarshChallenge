14aca98435707775db64eb18e316c3de24e7b29e
JohnMnyika <jmnyika514@gmail.com>
2021-07-31 12:20:13 +0300

    cHarsh challenge

---
+++ Exercise02.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Exercise02
+{
+    public Exercise02()
+    {
+
+
+            
+        {
+            ReadingInt();
+            int numbers;
+            Console.ReadLine();
+        }
+
+        static void ReadingInt( )
+        {
+
+            Console.Write("Enter a number: ");
+            Console.WriteLine();
+        }
+    } 
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regXp = "[a-z]+$";
+
+            string fruits = "apples";
+
+            string randomchar = "abc123xyz";
+
+            while (regXp != "[a-z]+$" && regXp.Contains("z")) 
+            {
+                    Console.Write("Enter a regular expression: ");
+                    
+                    regXp = Console.ReadLine();
+
+                Console.Write("Find a fruit I like: ");
+                fruits = Console.ReadLine();
+
+            }
+            if (fruits == "apples")
+            {
+                Console.Write("You got it");
+            }
+            else
+            
+
+
+            if (regXp == "[a-z]+$" && regXp.Contains("z"))
+            {
+
+                Console.Write("Enter a random character");
+            }
+
+
+            if (randomchar != "abc123xyz")
+            {
+                Console.WriteLine("Try again");
+            }
+
+            Console.WriteLine("");
+        }
+    }
+}
